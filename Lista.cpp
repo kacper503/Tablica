1381956795a6cc97ecf18f8b61d125fb1311b50a
Kacper Tworek <kt16127@student.ans-ns.edu.pl>
2025-10-18 15:18:32 +0200

    Lista dwukierunkowa

--- Lista.cpp
+++ Lista.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
 
 using namespace std;
+
+
+// Klasa listy dwukierunkowej
+class ListaDwukierunkowa {
+    private: 
+        struct Wezel {
+            int dane;
+            Wezel* poprzedni;
+            Wezel* nastepny;
+            Wezel(int wartosc) : dane(wartosc), poprzedni(nullptr), nastepny(nullptr) {}
+        };
+
+        Wezel* pierwszy; //pierwszy element
+        Wezel* ostatni; // ostatni element
+
+    public:
+    //Konstruktor
+    ListaDwukierunkowa() : pierwszy(nullptr), ostatni(nullptr) {}
+
+    //Destruktor
+    ~ListaDwukierunkowa() {
+        Wezel* aktualny = pierwszy;
+        while (aktualny) {
+            Wezel* nastepny = aktualny->nastepny;
+            delete aktualny;
+            aktualny = nastepny;
+        }
+    }
+};
